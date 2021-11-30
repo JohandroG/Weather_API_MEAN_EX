24178db9ca1f2ce64ba9a75585da505f0ff8f8ce
JohandroG <johandrogonzalez@gmail.com>
2021-11-30 13:22:53 -0600

    New App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SanjoseComponent } from './sanjose/sanjose.component';
+import { SeattleComponent } from './seattle/seattle.component';
+import { BurbankComponent } from './burbank/burbank.component';
+import { DallasComponent } from './dallas/dallas.component';
+import { WashingtonComponent } from './washington/washington.component';
+import { ChicagoComponent } from './chicago/chicago.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SanjoseComponent,
+    SeattleComponent,
+    BurbankComponent,
+    DallasComponent,
+    WashingtonComponent,
+    ChicagoComponent
   ],
   imports: [
     BrowserModule,
